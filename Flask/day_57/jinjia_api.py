bae4dfeb7048a683e662062f876c4ac835f05947
Prabhkirat Singh Sidhu <prabhkiratsinghsidhu2027@gmail.com>
2026-07-03 22:48:56 +0530

    habibi

--- Flask/day_57/jinjia_api.py
+++ Flask/day_57/jinjia_api.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def guess(name):
     gender_response = requests.get(f"https://api.genderize.io?name={name}")
     gender_data = gender_response.json()
-    gender = gender_data["gender"]
+    gender = gender_data["Gender"]
     return render_template("guess.html", person_name = name, gender = gender)
